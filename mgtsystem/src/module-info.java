1992a0f97ac4b2a282256fb08ab4c03062822e3b
Keea <zak.owasil@gmail.com>
2023-07-21 19:51:50 +0400

    Changes to module-info.java so that it runs on VS Code as well.

--- mgtsystem/src/module-info.java
+++ mgtsystem/src/module-info.java
@@ -6,4 +6,7 @@
  *
  */
 module mgtsystem {
+    //Added these two lines to remove the accessibilty error occurring when using VSC instead of Eclipse
+    requires java.desktop;
+    requires java.sql;
 }
