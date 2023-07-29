595a01fdaa2269fe61805c304893a7527db43dde
Stark <140642307+darthwook@users.noreply.github.com>
2023-07-29 19:05:27 +0400

    Add files via upload
    SQL for modified Modules table added
    (Project Not Merged- Prepared statement has to accommodate for logged in user)

---
+++ Timetable/Modules SQL.sql
@@ -0,0 +1,36 @@
+/*
+-- Query: SELECT * FROM student_ms.modules
+LIMIT 0, 1000
+
+-- Date: 2023-07-29 18:41
+*/
+INSERT INTO `` (`module_id`,`module_name`,`credits`,`course_name`,`day`,`time_slot`) VALUES (1,'Introduction to Literature',3,'Bachelor of Arts (BA)','MONDAY','08:00:00.000000');
+INSERT INTO `` (`module_id`,`module_name`,`credits`,`course_name`,`day`,`time_slot`) VALUES (2,'Calculus I',4,'Bachelor of Science (BS)','TUESDAY','09:30:00.000000');
+INSERT INTO `` (`module_id`,`module_name`,`credits`,`course_name`,`day`,`time_slot`) VALUES (3,'Principles of Management',3,'Bachelor of Business Administration (BBA)','WEDNESDAY','11:00:00.000000');
+INSERT INTO `` (`module_id`,`module_name`,`credits`,`course_name`,`day`,`time_slot`) VALUES (4,'Introduction to Programming',4,'Bachelor of Computer Science (BCS)','MONDAY','12:00:00.000000');
+INSERT INTO `` (`module_id`,`module_name`,`credits`,`course_name`,`day`,`time_slot`) VALUES (5,'Engineering Mechanics',3,'Bachelor of Engineering (BE)','MONDAY','13:00:00.000000');
+INSERT INTO `` (`module_id`,`module_name`,`credits`,`course_name`,`day`,`time_slot`) VALUES (6,'Educational Psychology',4,'Bachelor of Education (BEd)','MONDAY','14:00:00.000000');
+INSERT INTO `` (`module_id`,`module_name`,`credits`,`course_name`,`day`,`time_slot`) VALUES (7,'Painting Techniques',3,'Bachelor of Fine Arts (BFA)','SATURDAY','15:00:00.000000');
+INSERT INTO `` (`module_id`,`module_name`,`credits`,`course_name`,`day`,`time_slot`) VALUES (8,'Marketing Fundamentals',4,'Bachelor of Commerce (BCom)','WEDNESDAY','08:30:00.000000');
+INSERT INTO `` (`module_id`,`module_name`,`credits`,`course_name`,`day`,`time_slot`) VALUES (9,'Anatomy and Physiology',3,'Bachelor of Nursing (BN)','TUESDAY','11:30:00.000000');
+INSERT INTO `` (`module_id`,`module_name`,`credits`,`course_name`,`day`,`time_slot`) VALUES (10,'Social Work Ethics',4,'Bachelor of Social Work (BSW)','WEDNESDAY','08:00:00.000000');
+INSERT INTO `` (`module_id`,`module_name`,`credits`,`course_name`,`day`,`time_slot`) VALUES (11,'Architectural Design',3,'Bachelor of Architecture (BArch)','MONDAY','08:00:00.000000');
+INSERT INTO `` (`module_id`,`module_name`,`credits`,`course_name`,`day`,`time_slot`) VALUES (12,'Human Anatomy',4,'Bachelor of Medicine and Bachelor of Surgery (MBBS)','MONDAY','09:30:00.000000');
+INSERT INTO `` (`module_id`,`module_name`,`credits`,`course_name`,`day`,`time_slot`) VALUES (13,'Introduction to Law',3,'Bachelor of Law (LLB)','WEDNESDAY','11:00:00.000000');
+INSERT INTO `` (`module_id`,`module_name`,`credits`,`course_name`,`day`,`time_slot`) VALUES (14,'Introduction to Psychology',4,'Bachelor of Psychology (BPsych)','THURSDAY','12:00:00.000000');
+INSERT INTO `` (`module_id`,`module_name`,`credits`,`course_name`,`day`,`time_slot`) VALUES (15,'Environmental Science',3,'Bachelor of Environmental Science (BEnvSc)','THURSDAY','13:00:00.000000');
+INSERT INTO `` (`module_id`,`module_name`,`credits`,`course_name`,`day`,`time_slot`) VALUES (16,'Mass Communication',4,'Bachelor of Communication (BComm)','MONDAY','14:00:00.000000');
+INSERT INTO `` (`module_id`,`module_name`,`credits`,`course_name`,`day`,`time_slot`) VALUES (17,'Media and Society',3,'Bachelor of Media Studies (BMS)','THURSDAY','15:00:00.000000');
+INSERT INTO `` (`module_id`,`module_name`,`credits`,`course_name`,`day`,`time_slot`) VALUES (18,'Microeconomics',4,'Bachelor of Economics (BEcon)','WEDNESDAY','08:30:00.000000');
+INSERT INTO `` (`module_id`,`module_name`,`credits`,`course_name`,`day`,`time_slot`) VALUES (19,'Data Structures and Algorithms',3,'Bachelor of Information Technology (BIT)','THURSDAY','11:30:00.000000');
+INSERT INTO `` (`module_id`,`module_name`,`credits`,`course_name`,`day`,`time_slot`) VALUES (20,'Music Theory',4,'Bachelor of Music (BMus)','THURSDAY','08:00:00.000000');
+INSERT INTO `` (`module_id`,`module_name`,`credits`,`course_name`,`day`,`time_slot`) VALUES (21,'Public Health Policy',3,'Bachelor of Public Health (BPH)','FRIDAY','12:40:00.000000');
+INSERT INTO `` (`module_id`,`module_name`,`credits`,`course_name`,`day`,`time_slot`) VALUES (22,'International Relations',4,'Bachelor of International Relations (BIR)','FRIDAY','13:30:00.000000');
+INSERT INTO `` (`module_id`,`module_name`,`credits`,`course_name`,`day`,`time_slot`) VALUES (23,'Nursing Care Management',3,'Bachelor of Science in Nursing (BSN)','WEDNESDAY','11:00:00.000000');
+INSERT INTO `` (`module_id`,`module_name`,`credits`,`course_name`,`day`,`time_slot`) VALUES (24,'Sociology',4,'Bachelor of Social Sciences (BSS)','FRIDAY','12:30:00.000000');
+INSERT INTO `` (`module_id`,`module_name`,`credits`,`course_name`,`day`,`time_slot`) VALUES (25,'Graphic Design Principles',3,'Bachelor of Graphic Design (BDes)','FRIDAY','10:30:00.000000');
+INSERT INTO `` (`module_id`,`module_name`,`credits`,`course_name`,`day`,`time_slot`) VALUES (26,'Ethics and Philosophy',4,'Bachelor of Philosophy (BPhil)','SATURDAY','10:00:00.000000');
+INSERT INTO `` (`module_id`,`module_name`,`credits`,`course_name`,`day`,`time_slot`) VALUES (27,'Hospitality Operations',3,'Bachelor of Hospitality Management (BHM)','TUESDAY','09:00:00.000000');
+INSERT INTO `` (`module_id`,`module_name`,`credits`,`course_name`,`day`,`time_slot`) VALUES (28,'Sports Psychology',4,'Bachelor of Sports Science (BSSc)','TUESDAY','08:30:00.000000');
+INSERT INTO `` (`module_id`,`module_name`,`credits`,`course_name`,`day`,`time_slot`) VALUES (29,'Environmental Engineering',3,'Bachelor of Environmental Engineering (BEnvE)','TUESDAY','11:30:00.000000');
+INSERT INTO `` (`module_id`,`module_name`,`credits`,`course_name`,`day`,`time_slot`) VALUES (30,'Health Sciences Research',4,'Bachelor of Health Sciences (BHSc)','TUESDAY','09:30:00.000000');
---
+++ Timetable/src/db/ViewTimeTable.java
@@ -0,0 +1,145 @@
+package db;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+    import java.awt.FlowLayout;
+    import  java.awt.event.*;
+    import java.sql.Connection;
+    import java.sql.DriverManager;
+    import java.sql.ResultSet;
+    import java.sql.ResultSetMetaData;
+    import java.sql.SQLException;
+    import javax.swing.JFrame;
+    import javax.swing.JPanel;
+   import java.awt.BorderLayout;
+    import javax.swing.JTable;
+    import javax.swing.JButton;
+    import java.awt.Font;
+    import java.awt.LayoutManager;
+    import javax.swing.JScrollPane;
+    import javax.swing.table.*;
+     import java.awt.*; 
+     import javax.swing.*;
+     import java.sql.*; 
+     import javax.swing.event.*;
+
+    public class ViewTimeTable extends JFrame {
+
+    	private JPanel contentPane, jp_btn, jp_outer;
+    	private JTable table;
+    	private JButton btnDisplay, btnClear;
+    	private JScrollPane sp;
+
+    	
+    	   public ViewTimeTable() {
+    		setTitle("TimeTable");
+    		getContentPane().setLayout((LayoutManager) new BorderLayout());
+    		
+    		JButton btnDisplay = new JButton("View Timetable");
+    		btnDisplay.setFont(new Font("Arial", Font.BOLD, 11));
+
+    		table = new JTable();
+    		table.setBorder(null);
+    		table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+
+    		btnDisplay.addActionListener(new ActionListener() {
+    			public void actionPerformed(ActionEvent e) {
+    				Display();
+    			}
+    		}) ;
+
+    		btnClear = new JButton("Clear");
+    		btnClear.addActionListener(new ActionListener() {
+    			public void actionPerformed(ActionEvent e) {
+    				Clear();
+    			}
+    		}) ;
+    		
+    		jp_btn= new JPanel(new FlowLayout());
+    		jp_btn.add(btnDisplay);
+    		jp_btn.add(btnClear);
+     
+            // adding it to JScrollPane
+            sp = new JScrollPane(table);
+
+           contentPane = new JPanel(new GridLayout(1,1));
+           contentPane.add(sp);
+
+    		jp_outer = new JPanel(new BorderLayout());
+    		jp_outer.add(contentPane, BorderLayout.CENTER);
+    		jp_outer.add(jp_btn, BorderLayout.SOUTH);
+    
+    		getContentPane().add(jp_outer, BorderLayout.CENTER);
+    		
+    		
+    		
+    	}
+    	   
+    	   //Display Button Handler
+    	   private void Display() {
+    		  try{
+   	            Class.forName("com.mysql.cj.jdbc.Driver");
+   			    Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/student_ms","root","1234");
+   				
+   				/* SELECT modules.module_name, modules.day, modules.time_slot
+                   FROM modules
+                   RIGHT JOIN student ON student.course_name = modules.course_name
+                   WHERE student.username 
+                   IN(SELECT username FROM student WHERE username = "john_doe123")
+                   ORDER BY modules.day;*/
+
+   				String query = "select modules.module_id, modules.module_name, modules.day, modules.time_slot from modules RIGHT JOIN student ON student.course_name = modules.course_name WHERE student.username IN(SELECT username FROM student WHERE username = \"john_doe123\") ORDER BY modules.day, modules.time_slot;";
+   				java.sql.Statement st = con.createStatement();
+   				
+   				/* PREPARED STATEMENT (EDIT TO ACCOMMODATE LOGGED IN USER)
+   				java.sql.Statement st = con.createStatement();
+   			    PreparedStatement st = con.prepareStatement(query);
+                statement.setString(1, username); */
+                
+                //EXECUTE QUERY
+   				ResultSet rs = ((java.sql.Statement) st).executeQuery(query);
+   				ResultSetMetaData rsmd = rs.getMetaData();
+   				
+   				DefaultTableModel model = (DefaultTableModel) table.getModel();
+   				
+   				//fetch column info
+   				int cols = rsmd.getColumnCount();
+   				Object[] colName = {"Module ID", "Module Name","Day","Time"};
+   				for(int i =0; i<cols; i++){
+   					model.setColumnIdentifiers(colName); 
+   				
+   					String modId, modName, modDay, modTime;
+   					while(rs.next()) {
+   						modId = rs.getString(1);
+   						modName = rs.getString(2);
+   						modDay = rs.getString(3);
+   						modTime = (rs.getString(4)).substring(0,5);
+   						String[] row = {modId, modName,modDay, modTime};
+   						model.addRow(row);
+   					}
+   					st.close();
+   					con.close();
+   		}
+
+
+   			}  catch(ClassNotFoundException | SQLException  e1){
+   	              e1.printStackTrace();}
+   			
+   			}
+    	   
+    	   //Clear Button Handler
+    	   private void Clear() {
+    		   table.setModel(new DefaultTableModel());
+    	   }
+    	   }
+
+
+
+
+	
+
+	
+
+ 
